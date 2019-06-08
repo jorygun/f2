5ab5ddcea8e98cba47c13ea2b61829bce041b429
jorygun <jorygun@gmail.com>
2019-06-07 20:40:49 -0700

    fix

--- public/init.php
+++ public/init.php
@@ -1,7 +1,3 @@
 <?php
-
 #init file at known spot in web root to run the bootup file.
-$dir = dirname(__DIR__);
-echo "including $dir/config/boot.php";
-
-include_once("$dir/config/boot.php");
+include_once(dirname(__DIR__) . "/config/boot.php");
