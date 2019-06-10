d54eaf6a7c61b098a399a1da251524b08c3b6698
jorygun <jorygun@gmail.com>
2019-06-10 10:07:38 -0700

    fixed type

--- config/boot.php
+++ config/boot.php
@@ -41,7 +41,7 @@ ini_set('include_path',
 
 #add primary 
 require_once "mx-constants.php";
-require_once "mx-utilities.php"
+require_once "mx-utilities.php";
 
 #create container
 
@@ -50,15 +50,15 @@ require_once "mx-utilities.php"
 
 
 require_once  "f2_connect.php";
-require_once ('nav.class.php');
-require_once ($site_dir . '/composer-autoload');
-require_once ('setGlobals.php');
-require_once ('f2_constants.php'); 
-require_once ('Definitions.php');
+require_once 'nav.class.php';
+require_once $site_dir . '/composer-autoload';
+require_once 'setGlobals.php';
+require_once 'f2_constants.php'; 
+require_once 'Definitions.php';
 
 #build db
 $dbs = parse_ini_file('db.ini'); #gets all the connection params in an array
-require_once ('MyPDO.class.php');
+require_once 'MyPDO.class.php';
 $pdo = MyPDO::instance();
 
 
