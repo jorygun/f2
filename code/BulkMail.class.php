955cbf2599575462eb696527354031ba1fdccd99
jorygun <jorygun@gmail.com>
2019-06-13 09:23:48 -0700

    added BulkMail from old f2

---
+++ code/BulkMail.class.php
@@ -0,0 +1,70 @@
+<?php
+
+
+class BulkMail {
+
+	private $queue = SITEPATH . '/bulk_queue';
+	
+	
+	public function show_bulk_jobs(){
+		#looks for jobs in queue, and displays info about them
+	   		$queue = $this->queue;
+			$joblist = "<b>Jobs In Bulk Queue:</b><br>";
+			$jobs_in_queue = array_filter(scandir($queue), function($v){return substr($v,0,1) != '.';}); #files not staring with .
+		
+			 if (empty($jobs_in_queue)){return 'Nothing in queue';}
+			 
+			$joblist .= "<ul>";
+			foreach ($jobs_in_queue as $job){
+		 
+				$starttime = date('M d H:i  T',filemtime("$queue/$job"));
+				if ($c = strpos($job,'-cancelled')){
+					$jstat = 'cancelled';
+					$jobid = substr($job,0,$c);
+				}
+				elseif ($c = strpos($job,'-running')){
+					$jstat = 'running';
+					$jobid = substr($job,0,$c);
+				}
+				elseif ($c = strpos($job,'-error')){
+					$jstat = 'error';
+					$jobid = substr($job,0,$c);
+					$joblist .= "<li class='error'>$jobid is marked as an error";
+					continue;
+				}
+				else {
+					$jstat = 'queued';
+					$jobid = $job;
+				}
+				
+				$working = HOMEPATH . "/bmail/working_$jobid";
+				if (!is_dir($working)){
+					$joblist .= "<li class='error'>$jobid dir not found in bmail";
+					continue; #next job
+				}
+				
+				if (! file_exists("$working/list.txt") or
+					! file_exists("$working/message.txt") ){
+						$joblist .= "<li class='error'>$jobid: no list or message files found";
+						continue;
+				}
+				
+				$jcnt = `wc -l "$working/list.txt" | awk '{print $1;}'` ;
+				$jmsg = "$working/message.txt";
+				$jcancel = '';
+				$jsub = fgets(fopen("$jmsg", 'r')); #first line of message
+				if ($jstat == 'queued' or $jstat == 'running'){
+					$jcancel = "<button type='button' onClick='cancel_bulk($job)'>Cancel</button>";
+				}
+				$joblist .= "<li class='$jstat'>$jobid $jstat: '$jsub' to $jcnt recipients runs after: $starttime $jcancel<br>\n";
+		
+			}
+		
+			$joblist .= "</li>";
+			return $joblist;
+	}
+
+
+
+
+}
