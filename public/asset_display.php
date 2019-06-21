ff568754a9aba0a8228dbfaf72c63cac1599c894
techdude <john@Ayebook.local>
2019-06-21 10:03:38 -0700

    quick fix

--- public/asset_display.php
+++ public/asset_display.php
@@ -197,9 +197,14 @@ EOT;
 
 ##############################
 function myUrlEncode($string) {
-    $entities = array('%21', '%2A', '%27', '%28', '%29', '%3B', '%3A', '%40', '%26', '%3D', '%2B', '%24', '%2C', '%2F', '%3F', '%25', '%23', '%5B', '%5D');
-    $replacements = array('!', '*', "'", "(", ")", ";", ":", "@", "&", "=", "+", "$", ",", "/", "?", "%", "#", "[", "]");
-    return str_replace($entities, $replacements, urlencode($string));
+
+# $characters = array('!', '*', "'", "(", ")", ";", ":", "@", "&", "=", "+", "$", ",", "/", "?", "%", "#", "[", "]");
+ $characters = array('!', '*', "'", "(", ")", ";", ":", "@", "&", "=", "+", "$", ",",  "?", "%", "#", "[", "]");
+    #$entities = array('%21', '%2A', '%27', '%28', '%29', '%3B', '%3A', '%40', '%26', '%3D', '%2B', '%24', '%2C', '%2F', '%3F', '%25', '%23', '%5B', '%5D');
+    $entities = array_filter($characters,function ($c){return urlencode($c);});
+    
+   
+    return str_replace($characters, $entities, $string);
 }
 ?>
 
