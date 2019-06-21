34608ca09500969c6e16e8d90a80bcff4791ffb1
techdude <john@Ayebook.local>
2019-06-21 09:47:30 -0700

    quick fix

--- public/asset_display.php
+++ public/asset_display.php
@@ -113,7 +113,7 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
     if (!empty($row['link'])){
     	$url = $row['link'];
     	#$url_enc = str_replace('#','%23',$url);
-    	$url_enc = urlencode($url);
+    	$url_enc = myUrlEncode($url);
     	
     	$linkline = "Link to: <a href='$url_enc'>$url</a>";
 		$urllinked = "<a href='$url_enc' target='_blank'>$url</a>";
@@ -195,6 +195,12 @@ EOT;
             $credit .= "via " . $row['contributor'] ;
     }
 
+##############################
+function myUrlEncode($string) {
+    $entities = array('%21', '%2A', '%27', '%28', '%29', '%3B', '%3A', '%40', '%26', '%3D', '%2B', '%24', '%2C', '%2F', '%3F', '%25', '%23', '%5B', '%5D');
+    $replacements = array('!', '*', "'", "(", ")", ";", ":", "@", "&", "=", "+", "$", ",", "/", "?", "%", "#", "[", "]");
+    return str_replace($entities, $replacements, urlencode($string));
+}
 ?>
 
 <html><head>
