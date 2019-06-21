173d2aa38693534b95591b57f301eb78c8609347
techdude <john@Ayebook.local>
2019-06-20 19:07:00 -0700

    quick fix

--- public/news/index.php
+++ public/news/index.php
@@ -1,6 +1,7 @@
 <?php
 #$current = file_get_contents('latest_pointer.txt');
 #header("Location: $current");
+require_once $_SERVER['DOCUMENT_ROOT'] . '/init.php';;
 if (f2_security_below(2)){exit;}
 
 header("Location: news_latest");
