c2c1789f84d8f092b62bdf4a74452b1e19af86f7
techdude <john@Ayebook.local>
2019-06-24 21:08:48 -0700

    quick fix

--- public/index.php
+++ public/index.php
@@ -7,7 +7,8 @@
 	if (f2_security_below(0)){exit;}
 	
 	use digitalmx\flames\Definitions as Defs;
-	echo "From Defs: " . Defs::$stale_data_limit . BRNL;
+	echo "From Defs: " . Defs::$test . BRNL;
+	echo "From Defs: " . Defs::$stale_date_limit . BRNL;
 	exit;
 	
 	
