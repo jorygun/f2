981d0850513e1e23fc58d0bddfafa76fb6c52a84
techdude <john@Ayebook.local>
2019-07-07 16:58:56 -0700

    fixed

---
+++ public/sender.php
@@ -0,0 +1,16 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+
+
+	
+
+require_once '../config/init.php';
+
+
+use digitalmx\flames\Member;
+use digitalmx\flames\Messenger;
+
+$messenger = new Messenger(\MyPDO::instance);
+$messenger->sendLogins('john@digitalmx.com','here is the text');
+
