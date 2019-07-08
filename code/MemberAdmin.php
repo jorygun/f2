967c9ba70601f241034566c60861223a2c6b0b8f
techdude <john@Ayebook.local>
2019-07-08 13:16:49 -0700

    fixed

--- code/MemberAdmin.php
+++ code/MemberAdmin.php
@@ -58,7 +58,7 @@ class MemberAdmin {
 			<td colspan='2' >" . u\linkHref($row['user_email']) . "</td>";
          $login = $row['upw'] . $row['user_id'];
         $user_login_link = "https://amdflames.org/?s=$login";
-      $button_action = $uid . ',sendLogin';
+      $button_action = $uid . ",'sendLogin'";
         $o .=  "<td colspan='4'><a href='$user_login_link' target='_blank'>$user_login_link</a> 
         <button type='button' onClick='takeAction($button_action)'>Send Login</button></td></tr>";
 
