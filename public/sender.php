12eaeafc272c0ff763880460e72fadd73e14b614
techdude <john@Ayebook.local>
2019-07-07 17:00:43 -0700

    fixed

--- public/sender.php
+++ public/sender.php
@@ -10,7 +10,8 @@ require_once '../config/init.php';
 
 use digitalmx\flames\Member;
 use digitalmx\flames\Messenger;
+$pdo = \MyPDO::instance();
 
-$messenger = new Messenger(\MyPDO::instance());
+$messenger = new Messenger($pdo);
 $messenger->sendLogins('john@digitalmx.com','here is the text');
 
