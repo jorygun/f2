ff99a2987d362750c5048479a1d3eb757171cf2a
techdude <john@Ayebook.local>
2019-07-09 07:26:35 -0700

    fixed

--- code/MemberAdmin.php
+++ code/MemberAdmin.php
@@ -14,12 +14,12 @@ ini_set('error_reporting', -1);
 //BEGIN START
 
 #	require_once 'init.php';
-	use digitalmx\flames\Definitions as Defs;
 
-	use digitalmx as u;
-	use digitalmx\flames\Member;
-	use digitalmx\flames\Messenger;
-	use digitalmx\flames\DocPage;
+// 	use digitalmx\flames\Definitions as Defs;
+// 	use digitalmx as u;
+// 	use digitalmx\flames\Member;
+// 	use digitalmx\flames\Messenger;
+// 	use digitalmx\flames\DocPage;
 
 	
 //END START
