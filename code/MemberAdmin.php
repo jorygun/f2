f6a03615aedc68e2fd21e42165fc8083870af524
techdude <john@Ayebook.local>
2019-07-09 07:17:41 -0700

    fixed

--- code/MemberAdmin.php
+++ code/MemberAdmin.php
@@ -37,10 +37,10 @@ class MemberAdmin {
 	
 	
 	public function __construct(){
-		$this->pdo = \MyPDO::instance();
-#		$this->member = new Member ($this->pdo);
-		$this->page = new DocPage();
-		$this->messenger = new Messenger($this->pdo);
+// 		$this->pdo = \MyPDO::instance();
+// 		$this->member = new Member ($this->pdo);
+// 		$this->page = new DocPage();
+// 		$this->messenger = new Messenger($this->pdo);
 	}
 	
 	// this function just echos out the data in a list of found members.
