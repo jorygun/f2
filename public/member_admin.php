7aca0f664b7fc8bb5bc77d19aded0b9b8fe2772b
techdude <john@Ayebook.local>
2019-07-09 07:13:59 -0700

    fixed

--- public/member_admin.php
+++ public/member_admin.php
@@ -8,8 +8,8 @@ ini_set('error_reporting', -1);
 	use digitalmx\flames\Definitions as Defs;
 	use digitalmx\flames\DocPage;
 	use digitalmx\flames\MemberAdmin;
-// 	
-// 	$page = new DocPage();
+	
+	$page = new DocPage();
 // 	$admin = new MemberAdmin();
 // 	
 //    echo $page->getHead('Member Admin',0,['ajax']);
