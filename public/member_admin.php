692d6238e28fdb712b4e94716acb898efb783dac
techdude <john@Ayebook.local>
2019-07-16 17:57:00 -0700

    fixed member_update email

--- public/member_admin.php
+++ public/member_admin.php
@@ -2,42 +2,41 @@
 namespace digitalmx\flames;
 
 ini_set('display_errors', 1);
-ini_set('error_reporting', -1);
+
 //BEGIN START
 	require_once 'init.php';
 	use digitalmx\flames\Definitions as Defs;
 	use digitalmx\flames\DocPage;
 	use digitalmx\flames\MemberAdmin;
 	
+	$admin = new MemberAdmin($pdo);
 	$page = new DocPage();
-	$member = new Member;
-	$admin = new MemberAdmin($member);
+
 	
-     echo $page->getHead('Member Admin',0,['ajax']);
+   echo $page->startHead('Member Admin',['ajax']); 
  	echo $page ->startBody("Search for Member");
 
 	
 //END START
 	
 #display user data
-$uid = $_GET['id'] ?? '' ;
-if (!empty($uid)){
-	echo $admin->showUpdate($uid);
-}
 
 #display search results
-elseif (isset($_POST['search'])){
+if (isset($_POST['search'])){
 		echo $admin->listMembers ($_POST);
 }
 elseif (isset($_POST['Update'])){
 	if (empty($uid = $_POST['uid'])){
 		throw new Exception ('Attemp t update member with no uid in post');
 	}
+	
 	echo $admin->updateMember($_POST);
+	echo $admin->showUpdate($uid);
+}
+elseif ($uid = $_GET['id'] ?? '' ){
 	echo $admin->showUpdate($uid);
 }
 
 #show search screen
 echo $admin->showSearch();
-	
 
