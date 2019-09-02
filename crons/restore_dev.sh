b55d9d282efc004b112412b89777480489c907a1
techdude <john@Ayebook.local>
2019-09-02 12:02:26 -0700

    added restore_dev

---
+++ crons/restore_dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#reloads the f2-dev db from the main db backup
+#run daily or on demand
+
+
+PATH=/bin:/usr/bin:/usr/local/bin
+HOME=/usr/home/digitalm
+
+cd ${HOME}/backups
+
+
+# get latest daily file
+latest=$(ls -t daily.sql.* | head -1 )
+gunzip < "$latest" | mysql -udigitalm_6 -pfXFjb9ED digitalm_f2dev 
+
