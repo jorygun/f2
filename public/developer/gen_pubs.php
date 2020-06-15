d1551c0579af8a09cb87c9d0aa8a362faeef96de
techdude <john@Ayebook.local>
2020-06-15 14:59:58 -0700

    struggling with indexing

--- public/developer/gen_pubs.php
+++ public/developer/gen_pubs.php
@@ -40,7 +40,7 @@ if (! $file_index = json_decode (file_get_contents(FileDefs::news_index_json),tr
 }
 
 $pubindex = [];
-echo "Rebuild Pubs at " . date('M d Y H:i') . BRNL;
+echo "Rebuild Pubs version 9 at " . date('M d Y H:i') . BRNL;
 
  echo count($file_index) . " records in json index" . BRNL;
 
