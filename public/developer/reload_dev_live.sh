ebf8969103ba897f44b5816f44124bf1a1f68388
techdude <john@Ayebook.local>
2020-06-29 11:54:27 -0700

    fix reload

--- public/developer/reload_dev_live.sh
+++ public/developer/reload_dev_live.sh
@@ -53,11 +53,11 @@ else
 	/usr/local/bin/mysql -hdb158.pair.com -udigitalm_6 -p${PWdev} digitalm_f2dev < $sqltemp 2>/dev/null
 
 	JSON="/news/current/news_index.json"
-	echo -e $( ls -l "$SITEPATHL/$JSON")
+	ls -l "$SITEPATHL/$JSON"
 
 
 	cp "$SITEPATHL/$JSON" "$SITEPATHD/$JSON";
-	echo -e $(ls -l "$SITEPATHL/$JSON")
+	ls -l "$SITEPATHL/$JSON"
 
 # 	mysql -hdb158.pair.com -udigitalm_6 -p${PWdev} digitalm_f2dev <<EOT
 # 	DROP TABLE IF EXISTS backup_assets;
