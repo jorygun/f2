17dcbd75169ef05a585378bc0e49659ce9306515
techdude <john@Ayebook.local>
2020-08-09 13:19:04 -0700

    changed jform link to https

--- public/aq.php
+++ public/aq.php
@@ -34,12 +34,12 @@ if (isset($_POST['submit'])){
 </style>
 <link rel='stylesheet' href='/css/news4.css'>
 <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js'></script>
-  <script src="http://malsup.github.com/jquery.form.js"></script>
+<script src='https://malsup.github.com/jquery.form.js'></script>
 <script src='/js/help.js'></script>
 <script>
 
         // wait for the DOM to be loaded
-        $( document).ready (function() {
+        $(document).ready (function() {
             // bind 'myForm' and provide a simple callback function
             $('#asset_form').ajaxForm(function(responseText, statusText, xhr, $form) {
             	if(statusText != 'success') {alert ('Failed'); return false;}
