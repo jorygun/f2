079168bda0ef1c356f9c0375f6d13ffe8c40ff17
techdude <john@Ayebook.local>
2020-08-10 08:21:19 -0700

    fixed recent report sql

--- crons/Recent.php
+++ crons/Recent.php
@@ -23,7 +23,7 @@ use DigitalMx as u;
 /*  STARTUP */
 require_once  '../public/init.php';
 if (! @defined ('INIT')) { throw new Exception ("Init did not load"); }
-echo "init loaded"; exit;
+echo "init loaded";
 new Recent($container);
 
 
