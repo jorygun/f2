0e6dbf7fb1547eaf024c7cc9087e908c731bbf91
techdude <john@Ayebook.local>
2020-10-09 11:39:23 -0700

    fixed news source date

--- public/article_editor.php
+++ public/article_editor.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
    try {
    	$id = $article->saveArticle($post);
-   } catch Throwable ($e) {
+   } catch (Throwable $e) {
    	echo "Error saving article. " . $e->getMessage();
    	exit;
    }
